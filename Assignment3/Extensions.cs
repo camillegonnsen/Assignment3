8f45879cd7c98acc668e2edc04d37ca3f5458a5e
Camille <cgon@itu.dk>
2021-09-24 13:28:38 +0200

    Extensions made

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,32 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace BDSA2020.Assignment02
+
+namespace Assignment03
 {
     public static class Extensions
     {
+        public static void Print<T>(this IEnumerable<T> collection){
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items){
+            return items.SelectMany(item => item);
+            
+        }
+
+        public static IEnumerable<int> GreaterThan42DivisibleBy7(this IEnumerable<int> items){
+
+            return items.Where(item => item > 42 && item%7 == 0);
+        }
+
+        public static IEnumerable<int> LeapYear(this IEnumerable<int> items){
+
+            return items.Where(item => DateTime.IsLeapYear(item));
+        }
     }
 }
--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Assignment3
+namespace Assignment03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<HashSet<int>> xs = new HashSet<HashSet<int>> { new HashSet<int> { 1, 2, 3, 4, 5, 6 }, new HashSet<int> { 1, 2, 3, 4, 5, 6 } };
+            
+            //input.Flatten().Print();
+
+            int[] ys = new int[] {3, 10, 49, 54, 56};
+
+            //ys.GreaterThan42DivisibleBy7().Print();
+
+            ys.LeapYear().Print();
+            
         }
     }
 }
